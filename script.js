ba3e3d3522124428b1ede9314659345ca9b794fe
CRSJ <crossjawz@gmail.com>
2024-11-11 23:09:56 +0300

    commit

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-const test = 1
+
+let innerInput = document.getElementById('input');
+let mainButton = document.getElementById('button');
+let toDoArray = [];
+// let toDoText;
+// let inputText = innerInput.value;
+let id = 0;
+
+
+const addNewTask = function () {
+
+    if (innerInput.value !== null && innerInput.value !== '') { //почему если здесь inputText то нихера не работает?
+
+        let newTask = document.createElement('div');
+
+        newTask.innerHTML = innerInput.value;
+        newTask.setAttribute("id", id++)
+        document.body.append(newTask);
+        innerInput.value = '';
+        console.log(newTask);
+        toDoArray.push(newTask.id);
+        toDoArray.push(newTask.innerHTML);
+        console.log(toDoArray);
+
+    }
+};
+
+mainButton.addEventListener('click', addNewTask);
+innerInput.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        document.getElementById("button").click();
+    }
+});
